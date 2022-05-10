ec3381a6d5f45155a619ba31e7c8acb4aae14cef
John Syrinek <john.syrinek@gmail.com>
2022-05-10 20:56:48 +0000

    cleanup

--- cli/bundle.ts
+++ cli/bundle.ts
@@ -1,17 +1,3 @@
-// const { files, diagnostics } = await Deno.emit("./src/loop.ts", {
-//     bundle: "classic",
-//     compilerOptions: {
-//         module: 'commonjs'
-//     }
-// });
-//
-// Deno.writeTextFile('./loop.js', files['deno:///bundle.js'])
-// // console.log(files['deno:///bundle.js'])
-
-
-
-
-
 import * as esbuild from 'https://deno.land/x/esbuild@v0.14.38/mod.js'
 const result = await esbuild.transform(Deno.readTextFileSync('src/loop.ts'), { loader: 'ts' })
 console.log('result:', result)
